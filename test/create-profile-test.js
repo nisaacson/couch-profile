256c1e7e6c21354ba3cd770d939435505a4e043a
Noah Isaacson <clewfirst@gmail.com>
2013-02-26 22:09:49 -0500

    version bump

--- test/create-profile-test.js
+++ test/create-profile-test.js
@@ -1,68 +1,96 @@
-var async = require('async');
-var inspect = require('eyespect').inspector();
-var createUserProfile = require('../lib/createUserProfile');
-var should = require('should');
-var getDB = require('./getDB');
-var path = require('path');
-var fs = require('fs');
-var assert = require('assert');
-var configFilePath = path.join(__dirname, 'config.json');
-assert.ok(fs.existsSync(configFilePath), 'config file not found at path: ' + configFilePath);
-var config = require('nconf').argv().env().file({ file: configFilePath });
+var bcrypt = require('bcrypt-nodejs')
+var async = require('async')
+var inspect = require('eyespect').inspector()
+var createUserProfile = require('../lib/createUserProfile')
+var should = require('should')
+var getDB = require('./getDB')
+var path = require('path')
+var fs = require('fs')
+var assert = require('assert')
+var configFilePath = path.join(__dirname, 'config.json')
+assert.ok(fs.existsSync(configFilePath), 'config file not found at path: ' + configFilePath)
+var config = require('nconf').argv().env().file({ file: configFilePath })
 describe('create profile test', function () {
-  var db;
-  var email = 'foo@example.com';
+  var db
+  var email = 'foo@example.com'
   before(function (done) {
     getDB(config, function (err, reply) {
-      should.not.exist(err);
-      should.exist(reply);
-      db = reply;
+      should.not.exist(err)
+      should.exist(reply)
+      db = reply
       var removeData = {
         db: db,
         email: email
-      };
+      }
       removeIfNeeded(removeData, function (err) {
-        should.not.exist(err);
-        done();
-      });
-    });
+        should.not.exist(err)
+        done()
+      })
+    })
 
-  });
-  it('should create profile', function (done) {
+  })
+  it('should create profile with password', function (done) {
+    var password = 'fooPassword'
+    var data = {
+      db: db,
+      email: email,
+      password: password
+    }
+    createUserProfile(data, function (err, reply) {
+      should.not.exist(err)
+      reply.should.have.property('id')
+      reply.should.have.property('rev')
+      reply.should.have.property('hash')
+      var id = reply.id
+      db.get(id, function (err, reply) {
+        should.not.exist(err)
+        should.exist(reply)
+        email.should.eql(reply.email)
+        bcrypt.compare(password, reply.hash, function (err, reply) {
+          should.not.exist(err)
+          assert.ok(reply, 'bcrypt compare should be true')
+          done()
+        })
+      })
+    })
+  })
+  it('should create profile without password', function (done) {
+    var password = 'fooPassword'
     var data = {
       db: db,
       email: email
-    };
+    }
     createUserProfile(data, function (err, reply) {
-      should.not.exist(err);
-      reply.should.have.property('id');
-      reply.should.have.property('rev');
-      var id = reply.id;
+      should.not.exist(err)
+      reply.should.have.property('id')
+      reply.should.have.property('rev')
+      reply.should.have.property('hash')
+      var id = reply.id
       db.get(id, function (err, reply) {
-        should.not.exist(err);
-        should.exist(reply);
-        email.should.eql(reply.email);
-        done();
-      });
-    });
-  });
-});
+        should.not.exist(err)
+        should.exist(reply)
+        email.should.eql(reply.email)
+        done()
+      })
+    })
+  })
+})
 
 
 function removeIfNeeded(data, callback) {
-  var db = data.db;
-  var email = data.email;
+  var db = data.db
+  var email = data.email
   db.view('user_profile/byEmail', {key: email}, function (err, docs) {
-    if (err) { return callback(err); }
+    if (err) { return callback(err) }
     if (docs.length === 0) {
-      return callback();
-    };
+      return callback()
+    }
     async.forEachSeries(
       docs,
       function (doc, cb) {
-        var id = doc.value._id;
-        var rev = doc.value._rev;
-        db.remove(id, rev, cb);
-      }, callback);
-  });
+        var id = doc.value._id
+        var rev = doc.value._rev
+        db.remove(id, rev, cb)
+      }, callback)
+  })
 }
